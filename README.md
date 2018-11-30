44b4e11761db8369b2a432ce10bdfc7db53dc854
Iury Lamonie <iury.escolar@hotmail.com>
2018-11-30 00:09:28 -0300

    Falta coisas bestas.

---
+++ README.md
@@ -0,0 +1,53 @@
+# Autores
+
+Iury Lamonie
+& 
+Leonardo Igor
+
+# 1. Introdução:
+
+Este projeto contém a implementação de dois dictionary Abstract Data Type (ADT) baseada em uma Array List.
+
+# 2. O programa de testes:
+
+Foi utilizado uma copia do arquivo de testes disponibilizado no SIGAA. O conteudo do arquivo foi copiado para o ```main.cpp```.
+
+Para compilar o arquivo de testes, utilize o comando ```make``` dentro da pasta ```list``` e em seguida o comando ```./bin/main``` para rodar o programa.
+
+# 2. Makefile/Compiling:
+
+Verifica se o setup está configurado, caso contrario, fará um novo setup chamando "make dir" e compila. Ele chama "make all" internamente.
+
+```
+make
+```
+
+Você também pode passar argumentos para o compilador. Como o exemplo abaixo:
+```
+make args="-D something=212"
+```
+Para ativar o modo debug, faça:
+
+```
+make args="-D DEBUG"
+```
+
+Fará tods os diretórios de trabalho. Você pode fazer isso manualmente ou automaticamente se você usar "make".
+```
+make dir
+```
+
+Limpa todo o diretorio de trabalho, irá apagar todos os arquivos contidos em bin e docs
+```
+make clean
+```
+
+Fará a documentação para o projeto atual.
+```
+make doxy
+```
+
+Irá chamar "make clear" e "make dir"
+```
+make fresh
+```
--- include/dal.h
+++ include/dal.h
@@ -1,12 +1,12 @@
 #ifndef _DAL_H_
 #define _DAL_H_
 
-#include <functional> //< std::equal_to
+#include <functional> //< std::less
 #include <iostream> //< std::ostream
 #include <algorithm> //< std::swap
 template < typename Key, //< Tipo da chave. 
 		   typename Data, //< Tipo da informação.
-		   typename KeyComparator = std::equal_to< Key > > //< Function de comparação de chaves.
+		   typename KeyComparator = std::less< Key > > //< Function de comparação de chaves.
 class DAL
 {
 protected:
@@ -227,12 +227,173 @@ public:
 
 template < typename Key, //< Tipo da chave. 
 		   typename Data, //< Tipo da informação.
-		   typename KeyComparator = std::equal_to< Key > > //< Function de comparação de chaves.
+		   typename KeyComparator = std::less< Key > > //< Function de comparação de chaves.
 class DSAL : public DAL< Key, Data, KeyComparator >
 {
 public:
-	DSAL( int _MaxSz) : DAL< Key, Data, KeyComparator >( _MaxSz ) { /*Empty*/}
-	~DSAL();
+	DSAL( int _MaxSz = DAL< Key, Data, KeyComparator >::SIZE ) : DAL< Key, Data, KeyComparator >( _MaxSz ) { /*Empty*/}
+	//virtual ~DSAL();
+	
+	/**
+	 * @brief      Insere o par associado, chave e informação, no dicionário D.
+	 * @param[in]  _newKey   A chave.
+	 * @param[in]  _newInfo  A informação.
+	 *
+	 * @return     True se foi inserido, false caso contrário.
+	 */
+	bool insert( const Key & _novaId, const Data & _novaInfo )
+	{
+		// Verifica se a lista está vazia.
+		if( this->empty() )
+		{
+			this->mpt_data[0].id = _novaId;
+			this->mpt_data[0].info = _novaInfo;
+			++this->m_lenght;
+			return true;
+		}
+
+		// Verifica se a chave não existe na lista.
+		if( this->_search( _novaId ) == this->m_lenght )
+		{
+			KeyComparator compFunc;
+			// Percorre a tabela pra encontrar a primeira chave maior que a nova chave..
+			for( int i = 0; i < this->m_lenght; ++i )
+			{
+				// Verifica se a chave é maior que a nova chave.
+				if( compFunc( _novaId, this->mpt_data[i].id ) )
+				{
+					// Empurra toda tabela.
+					for( int j = this->m_lenght; j > i; --j )
+					{
+						std::swap( this->mpt_data[j], this->mpt_data[j-1] );
+					}
+
+					this->mpt_data[i].id = _novaId;
+					this->mpt_data[i].info = _novaInfo;
+					++this->m_lenght;
+					return true;
+					
+				}
+			}
+			// É a maior chave da lista.
+			this->mpt_data[this->m_lenght].id = _novaId;
+			this->mpt_data[this->m_lenght].info = _novaInfo;
+			++this->m_lenght;
+			std::cout << "Chave " << _novaId << " Adicionada!" << std::endl;
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @brief      Remove e retorna do dicionário a informação associada à chave.
+	 *
+	 * @param[in]  _x    A chave.
+	 * @param      _s    A informação.
+	 *
+	 * @return     True se removeu, false caso contrário.
+	 */
+	bool remove( const Key & _x, Data & _s)
+	{
+		if( !this->empty() )
+		{
+			//Verifica se a não chave existe.
+			if( ! ( this->_search(_x) == this->m_lenght ) )
+			{
+				// Percorre do indice da chave procurada ate o fim da lista.
+				for( int i = this->_search( _x ); i < this->m_lenght; ++i)
+				{
+					// Joga a chave procura no fim da lista.
+					std::swap( this->mpt_data[i], this->mpt_data[i + 1] );
+				}
+				_s = this->mpt_data[this->m_lenght].info;
+				--this->m_lenght;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @brief      Recuperam do dicionário a menor chave.
+	 *
+	 * @return     A menor chave.
+	 */
+	Key min() const { return this->mpt_data[0].id; }
+
+	/**
+	 * @brief      Recuperam do dicionário a maior chave.
+	 *
+	 * @return     A maior chave.
+	 */
+	Key max() const { return this->mpt_data[this->m_lenght - 1].id; }
+
+	/**
+	 * @brief      Recupera do dicionário a chave que seja imediatamente posterior a outra chave.
+	 *
+	 * @param[in]  _x    Outra chave.
+	 * @param      _y    Chave posterior.
+	 *
+	 * @return     True se a chave posterior existe, false caso contrário.
+	 */
+	bool sucessor( const Key & _x, Key & _y )
+	{
+		if( !this->empty() )
+		{
+			int i = this->_search( _x );
+
+			//Verifica se a chave existe na lista
+			//E se a chave procurada não é o ultimo elemento.
+			if( !( i == this->m_lenght ) && ( i < ( this->m_lenght - 1 ) ) )
+			{
+				_y = this->mpt_data[i + 1].id;
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * @brief      Recupera do dicionário a chave que seja imediatamente anterior a outra chave.
+	 *
+	 * @param[in]  _x    Outra chave.
+	 * @param      _y    Chave anterior.
+	 *
+	 * @return     True se a chave anterior existe, false caso contrário.
+	 */
+	bool predecessor( const Key & _x, Key & _y )
+	{
+		if( !this->empty() )
+		{
+			int i = this->_search( _x );
+
+			//Verifica se a chave existe na lista
+			//E se a chave procurada não é o primeiro elemento.
+			if( !( i == this->m_lenght ) && ( i > 0 ) )
+			{
+				_y = this->mpt_data[i - 1].id;
+				return true;
+			}
+		}
+		return false;
+	}
+private:
+	int _search( const Key & _x ) const
+	{
+		int mid;
+		int i(0); //< Indice do elemento procurado
+		int l = this->m_lenght - 1;// last elemento.
+	 	while( i <= l )
+	 	{
+	 		mid = (i + l) / 2;
+	 		if(this->mpt_data[mid].id == _x) return mid;
+	 		else if(_x < this->mpt_data[mid].id) l = mid - 1;
+	 		else i = mid + 1;
+	 	}
+	 	return this->m_lenght; //< A key não foi encontrada
+	}
 	
 };
 #endif
